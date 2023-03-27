3dc093d7d6f071a2d623735e127a458ddc23dc6b
vib3sf <velobaton228@gmail.com>
2023-03-27 11:31:13 +0300

    made question and choice models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=100)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
