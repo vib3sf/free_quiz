5b9670c591bd20ee7e2c1da449d0124b7cb5d3b5
vib3sf <velobaton228@gmail.com>
2023-04-07 12:35:09 +0300

    make cbv edit

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,13 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('create_poll/', login_required(CreatePoll.as_view()), name='create_poll'),
+    path('edit_poll/<int:poll_id>', login_required(EditPoll.as_view()), name='edit_poll'),
     path('poll/<int:poll_id>/', login_required(ShowPoll.as_view()), name='show_poll'),
     path('create_question/<int:poll_id>', login_required(CreateQuestion.as_view()), name='create_question'),
-    path('edit_question/<int:question_id>/', edit_question, name='edit_question'),
+    path('edit_question/<int:question_id>/', login_required(EditQuestion.as_view()), name='edit_question'),
     path('delete_question/<int:question_id>/', delete_question, name='delete_question'),
     path('create_choice/<int:question_id>/', login_required(CreateChoice.as_view()), name='create_choice'),
-    path('edit_choice/<int:choice_id>/', edit_choice, name='edit_choice'),
+    path('edit_choice/<int:choice_id>/', login_required(EditChoice.as_view()), name='edit_choice'),
     path('delete_choice/<int:choice_id>/', delete_choice, name='delete_choice'),
     path('vote/<int:choice_id>/', vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView
-from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
@@ -14,12 +13,19 @@ def index(request):
 
 class CreatePoll(CreateView):
     form_class = PollForm
-    model = Poll
     template_name = 'polls/create_or_edit_poll.html'
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+
+class EditPoll(UpdateView):
+    form_class = PollForm
+    template_name = 'polls/create_or_edit_poll.html'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Poll, id=self.kwargs['poll_id'])
 
 
 class ShowPoll(DetailView):
@@ -40,20 +46,15 @@ class CreateQuestion(CreateView):
         return super().form_valid(form)
 
 
-@login_required
-def edit_question(request, question_id):
-    if request.method == 'POST':
-        form = QuestionForm(request.POST)
-        question = get_object_or_404(Question, id=question_id)
-        if form.is_valid():
-            question.question_text = form.cleaned_data['question_text']
-            question.save()
-            return redirect('show_poll', get_object_or_404(Question, id=question_id).poll_id)
-    form = QuestionForm()
-    context = {
-        "form": form
-    }
-    return render(request, "polls/create_or_edit_question.html", context)
+class EditQuestion(UpdateView):
+    form_class = QuestionForm
+    template_name = 'polls/create_or_edit_question.html'
+
+    def get_success_url(self):
+        return reverse('show_poll', kwargs={'poll_id': self.get_object().poll_id})
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Question, id=self.kwargs['question_id'])
 
 
 @login_required
@@ -68,28 +69,23 @@ class CreateChoice(CreateView):
     template_name = 'polls/create_or_edit_choice.html'
 
     def get_success_url(self):
-        return reverse('show_poll',
-                       kwargs={'poll_id': get_object_or_404(Question, id=self.kwargs['question_id']).poll_id})
+        return reverse('show_poll', kwargs={'poll_id': get_object_or_404(
+            Question, id=self.kwargs['question_id']).poll_id})
 
     def form_valid(self, form):
         form.instance.question_id = self.kwargs['question_id']
         return super().form_valid(form)
 
 
-@login_required
-def edit_choice(request, choice_id):
-    if request.method == 'POST':
-        form = ChoiceForm(request.POST)
-        choice = get_object_or_404(Choice, id=choice_id)
-        if form.is_valid():
-            choice.choice_text = form.cleaned_data['choice_text']
-            choice.save()
-            return redirect('show_poll', choice.question.poll_id)
-    form = ChoiceForm()
-    context = {
-        "form": form
-    }
-    return render(request, "polls/create_or_edit_choice.html", context)
+class EditChoice(UpdateView):
+    form_class = ChoiceForm
+    template_name = 'polls/create_or_edit_question.html'
+
+    def get_success_url(self):
+        return reverse('show_poll', kwargs={'poll_id': self.get_object().question.poll_id})
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Choice, id=self.kwargs['choice_id'])
 
 
 @login_required
