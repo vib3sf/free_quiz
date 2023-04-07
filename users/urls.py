2c53d5e1700ca67e8fff090d27d90b63ec3c2770
vib3sf <velobaton228@gmail.com>
2023-04-07 14:18:38 +0300

    make view class register

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-    path('registration/', register, name='register'),
+    path('registration/', Register.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', logout, name='logout'),
     path('profile/', profile, name='profile'),
--- users/views.py
+++ users/views.py
@@ -1,21 +1,16 @@
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
 import django.contrib.auth as log
-from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
 from .forms import UserRegisterForm
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created {username}!')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+class Register(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+
+    def get_success_url(self):
+        return reverse('home')
 
 
 @login_required
