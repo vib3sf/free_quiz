c80d4f77123ced189afead90fc6b116312f4c075
vib3sf <velobaton228@gmail.com>
2023-04-08 16:19:52 +0300

    add completed polls in profile page

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('registration/', Register.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', logout, name='logout'),
-    path('profile/', profile, name='profile'),
+    # path('profile/', profile, name='profile'),
+    path('profile/', login_required(Profile.as_view()), name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
+from polls.models import Poll
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 import django.contrib.auth as log
 from django.shortcuts import render, redirect, reverse
 from .forms import UserRegisterForm
@@ -13,12 +15,17 @@ class Register(CreateView):
         return reverse('home')
 
 
-@login_required
-def profile(request):
-    context = {
-        'polls': request.user.poll_set.all(),
-    }
-    return render(request, 'users/profile.html', context)
+class Profile(ListView):
+    template_name = "users/profile.html"
+    model = Poll
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'user_polls': self.request.user.poll_set.all(),
+            'completed_polls': Poll.objects.filter(question__choice__vote__voter=self.request.user)
+        })
+        return context
 
 
 @login_required
