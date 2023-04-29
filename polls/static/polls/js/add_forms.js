79b8cf749bdd8a8bc2e3c6710359366fa51d4cdf
vib3sf <velobaton228@gmail.com>
2023-04-29 23:24:37 +0300

    final front

--- polls/static/polls/js/add_forms.js
+++ polls/static/polls/js/add_forms.js
@@ -1,6 +1,6 @@
 let count = 1;
 
-function addTest() {
+function addQuestion() {
     const question_container = document.getElementById("question_container");
 
     const question_div = document.createElement("div");
@@ -10,11 +10,13 @@ function addTest() {
 
     question_input.type = "text";
     question_input.name = "question_" + count++;
+    question_input.placeholder = "Input question";
     question_div.appendChild(question_input);
 
     const remove_question = document.createElement("button");
     remove_question.type = "button";
     remove_question.innerHTML = "-";
+    remove_question.className = "btn btn-outline-danger"
     remove_question.onclick = function () {
         question_div.remove();
     }
@@ -22,6 +24,7 @@ function addTest() {
     const add_choice = document.createElement("button");
     add_choice.type = "button";
     add_choice.innerHTML = "Add choice";
+    add_choice.className = "btn btn-outline-info"
 
     let choice_count = 1;
     add_choice.onclick = function() {
@@ -30,10 +33,12 @@ function addTest() {
         const choice_input = document.createElement("input");
         choice_input.type = "text";
         choice_input.name = "choice_" + question_input.name + "_" + choice_count++;
+        choice_input.placeholder = "Input choice";
 
         const remove_choice = document.createElement("button");
         remove_choice.type = "button";
         remove_choice.innerHTML = "-";
+        remove_choice.className = "btn btn-outline-danger"
 
         remove_choice.onclick = function () {
             choice_div.remove();
