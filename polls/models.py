9b58f4e2c29742c7506a56739c1b92f5b60ab570
vib3sf <velobaton228@gmail.com>
2023-05-05 10:39:47 +0300

    blank description field in poll model and hiding in template if description empty

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 class Poll(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
     can_revote = models.BooleanField()
