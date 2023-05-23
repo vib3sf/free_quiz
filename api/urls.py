686c01d1369b98aba323a54327da91729d484530
vib3sf <velobaton228@gmail.com>
2023-05-23 22:05:02 +0300

    api register/login/logout

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import PollListCreateAPIView, PollDeleteAPIView
 
 urlpatterns = [
     path('create/', PollListCreateAPIView.as_view(), name='api_create'),
     path('delete/<int:pk>', PollDeleteAPIView.as_view(), name='api_delete'),
+    path('accounts/', include('rest_registration.api.urls'), name='accounts'),
 ]
