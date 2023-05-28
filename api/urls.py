2a3d6de302f24cf6a6897edb3dbd7c65891e1002
vib3sf <velobaton228@gmail.com>
2023-05-28 21:10:27 +0300

    rename url create to polls

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include, re_path
 from .views import PollListCreateAPIView, PollDeleteAPIView, VoteCreateAPIVIew
 
 urlpatterns = [
-    path('create/', PollListCreateAPIView.as_view(), name='api_create'),
+    path('polls/', PollListCreateAPIView.as_view(), name='api_create'),
     path('vote/', VoteCreateAPIVIew.as_view(), name='api_vote'),
     path('delete/<int:pk>/', PollDeleteAPIView.as_view(), name='api_delete'),
     path('accounts/', include('rest_registration.api.urls'), name='api_accounts'),
